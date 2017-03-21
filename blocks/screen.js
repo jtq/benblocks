6b7d65bdf89448ee50bb75bf9aabae461aa697b4
jtq <jamestpalmer1979@gmail.com>
2017-03-21 23:33:26 +0000

    Started refactored screen into more reusable OutputBlock class

--- blocks/screen.js
+++ blocks/screen.js
@@ -19,89 +19,86 @@
  *    B7: B6 on other Pico
  */
 
+function OutputBlock(serialPort) {
 
-// No block data
+  this.serialPort = serialPort;
 
-var screenReady = null;
-
-function setReceiving(receiving) {
-  if(receiving) {
-    digitalWrite(LED1, 1);  // Red = receiving
-    digitalWrite(LED2, 0);  // Green = listening
-  }
-  else {
-    digitalWrite(LED1, 0);  // Red = receiving
-    digitalWrite(LED2, 1);  // Green = listening
-  }
-}
-
-function setUp() {
-
-  USB.setConsole();
-
-  // LCD dimensions
-  var screen = {
+  this.dimensions = { // LCD dimensions
     width: 84,
     height: 48
   };
 
+  this.screenReady = null;  // Screen-initialised Promise
+  this.buffer = '';
+}
+
+OutputBlock.prototype.setBusy = function(busy) {
+  digitalWrite(LED1, 0+busy);  // Red = busy
+  digitalWrite(LED2, 0+(!busy));  // Green = waiting
+};
+
+OutputBlock.prototype.onData = function(data) { // Data received through serial connection
+  this.setBusy(true);
+
+  //console.log("Received", typeof data, data);
+
+  this.buffer += data;
+
+  if(data.indexOf('\u0004') !== -1) { // ctrl+D - End of Transmission character
+    this.screenReady.then(this.processBuffer.bind(this));
+
+    this.setBusy(false);
+  }
+};
+
+OutputBlock.prototype.processBuffer = function(g) {
+  g.clear();
+
+  var obj = null;
+  try {
+    obj = JSON.parse(this.buffer);
+    obj.imageValue.buffer = E.toArrayBuffer(atob(obj.imageValue.buffer));
+  }
+  catch(e) {
+    console.error("Error parsing JSON: -->", obj, "<--");
+  }
+
+  var xOffset = Math.floor((this.dimensions.width - obj.imageValue.width) / 2);
+  var yOffset = Math.floor((this.dimensions.height - obj.imageValue.height) / 2);
+  g.drawImage(obj.imageValue, xOffset, yOffset);
+  // send the graphics to the display
+  g.flip();
+
+  this.buffer = '';  // And reset buffer
+};
+
+OutputBlock.prototype.onDisconnect = function(e) {  // On loss of connection, clear screen
+  //console.log('Disconnect');
+
+  this.screenReady.then(function(g) {
+    g.clear();
+    g.flip();
+  });
+};
+
+OutputBlock.prototype.setUp = function() {
+
+  USB.setConsole();
+
   /**** Set up inputs ****/
   pinMode(B3, 'input_pulldown');  // Listen for connection from earlier block
 
-  Serial1.setup(9600, { tx:B6, rx:B7 });  // Data connection
-  setReceiving(false);
+  this.serialPort.setup(9600, { tx:B6, rx:B7 });  // Data connection
+  this.setBusy(false);
 
-  var buffer = '';
-  Serial1.on('data', function (data) {
+  this.serialPort.on('data', this.onData.bind(this));
 
-    setReceiving(true);
-
-    //console.log("Received", typeof data, data);
-
-    buffer += data;
-
-    if(data.indexOf('\u0004') !== -1) { // ctrl+D - End of Transmission character
-      screenReady.then(function(g) {
-        g.clear();
-
-        var obj = null;
-        try {
-          obj = JSON.parse(buffer);
-          obj.imageValue.buffer = E.toArrayBuffer(atob(obj.imageValue.buffer));
-        }
-        catch(e) {
-          console.error("Error parsing JSON: -->", obj, "<--");
-        }
-
-        var xOffset = Math.floor((screen.width - obj.imageValue.width) / 2);
-        var yOffset = Math.floor((screen.height - obj.imageValue.height) / 2);
-        g.drawImage(obj.imageValue, xOffset, yOffset);
-        // send the graphics to the display
-        g.flip();
-
-        buffer = '';  // And reset buffer
-      });
-
-      setReceiving(false);
-    }
-
-  });
-
-  setWatch(function(e) {  // On loss of connection, clear screen
-
-    //console.log('Disconnect');
-
-    screenReady.then(function(g) {
-      g.clear();
-      g.flip();
-    });
-
-  }, B3, { repeat: true, edge: 'falling', debounce:500 });  
+  setWatch(this.onDisconnect.bind(this), B3, { repeat: true, edge: 'falling', debounce:500 });  
 
 
   /**** Set up outputs ****/
 
-  screenReady = new Promise(function(resolve, reject) {   // screen
+  this.screenReady = new Promise(function(resolve, reject) {   // screen
     A5.write(0); // GND
     A7.write(1); // VCC
 
@@ -117,6 +114,8 @@ function setUp() {
       resolve(g);
     });
   });
-}
+};
 
-E.on('init', setUp);
+var block = new OutputBlock(Serial1);
+
+E.on('init', block.setUp.bind(block));
