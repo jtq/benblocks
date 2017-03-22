919f1784a6799bea8e566554c5c56056f26e7357
jtq <jamestpalmer1979@gmail.com>
2017-03-22 00:14:45 +0000

    Refactored OutputBlock to split it into OutputBlock (common output-block connectivity) and ScreenBlock (screen-specific functionality)

--- blocks/screen.js
+++ blocks/screen.js
@@ -19,17 +19,12 @@
  *    B7: B6 on other Pico
  */
 
+/**** Define general "Output" block class, containing common connectivity setup/event-handling (to be moved into a separate module when possible) ****/
 function OutputBlock(serialPort) {
 
   this.serialPort = serialPort;
 
-  this.dimensions = { // LCD dimensions
-    width: 84,
-    height: 48
-  };
-
-  this.screenReady = null;  // Screen-initialised Promise
-  this.buffer = '';
+  this.buffer = ''; // Buffer for de-chunking incoming serial data
 }
 
 OutputBlock.prototype.setBusy = function(busy) {
@@ -42,43 +37,21 @@ OutputBlock.prototype.onData = function(data) { // Data received through serial 
 
   //console.log("Received", typeof data, data);
 
-  this.buffer += data;
+  this.buffer += data;  // De-chunk into buffer, and process whole buffer once EOT control character is received
 
   if(data.indexOf('\u0004') !== -1) { // ctrl+D - End of Transmission character
-    this.screenReady.then(this.processBuffer.bind(this));
-
+    this.processBuffer(this.buffer);
+    this.buffer = '';  // And reset buffer
     this.setBusy(false);
   }
 };
 
-OutputBlock.prototype.processBuffer = function(g) {
-  g.clear();
-
-  var obj = null;
-  try {
-    obj = JSON.parse(this.buffer);
-    obj.imageValue.buffer = E.toArrayBuffer(atob(obj.imageValue.buffer));
-  }
-  catch(e) {
-    console.error("Error parsing JSON: -->", obj, "<--");
-  }
-
-  var xOffset = Math.floor((this.dimensions.width - obj.imageValue.width) / 2);
-  var yOffset = Math.floor((this.dimensions.height - obj.imageValue.height) / 2);
-  g.drawImage(obj.imageValue, xOffset, yOffset);
-  // send the graphics to the display
-  g.flip();
-
-  this.buffer = '';  // And reset buffer
+OutputBlock.prototype.processBuffer = function(buffer) {  // Process complete (de-chunked) data object
+  // console.log('Buffer to process:', buffer);
 };
 
-OutputBlock.prototype.onDisconnect = function(e) {  // On loss of connection, clear screen
+OutputBlock.prototype.onDisconnect = function(e) {  // On loss of connection
   //console.log('Disconnect');
-
-  this.screenReady.then(function(g) {
-    g.clear();
-    g.flip();
-  });
 };
 
 OutputBlock.prototype.setUp = function() {
@@ -95,8 +68,28 @@ OutputBlock.prototype.setUp = function() {
 
   setWatch(this.onDisconnect.bind(this), B3, { repeat: true, edge: 'falling', debounce:500 });  
 
+};
 
-  /**** Set up outputs ****/
+/**** Define specific "Screen" block class that descends from OutputBlock ****/
+function ScreenBlock(inputSerialPort) {
+  OutputBlock.call(this, inputSerialPort);
+
+  this.dimensions = { // LCD dimensions
+    width: 84,
+    height: 48
+  };
+
+  this.screenReady = null;  // Screen-initialised Promise
+}
+
+ScreenBlock.prototype = Object.create(OutputBlock.prototype);
+ScreenBlock.prototype.constructor = ScreenBlock;
+
+ScreenBlock.prototype.setUp = function() {
+
+  OutputBlock.prototype.setUp.call(this);
+
+  /**** Set up screen ****/
 
   this.screenReady = new Promise(function(resolve, reject) {   // screen
     A5.write(0); // GND
@@ -116,6 +109,35 @@ OutputBlock.prototype.setUp = function() {
   });
 };
 
-var block = new OutputBlock(Serial1);
+ScreenBlock.prototype.processBuffer = function(buffer) {
+  this.screenReady.then(function(g) {
+    g.clear();
+
+    var obj = null;
+    try {
+      obj = JSON.parse(buffer);
+      obj.imageValue.buffer = E.toArrayBuffer(atob(obj.imageValue.buffer));
+    }
+    catch(e) {
+      console.log("Error parsing buffer\Buffer: -->", buffer, "<--\nJSON: -->", obj, "<--");
+    }
+
+    var xOffset = Math.floor((this.dimensions.width - obj.imageValue.width) / 2);
+    var yOffset = Math.floor((this.dimensions.height - obj.imageValue.height) / 2);
+    g.drawImage(obj.imageValue, xOffset, yOffset);
+    // send the graphics to the display
+    g.flip();
+  }.bind(this));
+};
+
+ScreenBlock.prototype.onDisconnect = function(e) {  // On loss of connection, clear screen
+  this.screenReady.then(function(g) {
+    g.clear();
+    g.flip();
+  });
+};
+
+
+var block = new ScreenBlock(Serial1);
 
 E.on('init', block.setUp.bind(block));
